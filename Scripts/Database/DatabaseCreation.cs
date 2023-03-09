d6a56059e0b57f49b6ab1c956811e67252aa2c06
Demian Ferreyra <demiancra@gmail.com>
2023-03-08 21:54:45 -0300

    [feature] responses and new database file for words that are not discovered

--- Scripts/Database/DatabaseCreation.cs
+++ Scripts/Database/DatabaseCreation.cs
@@ -15,6 +15,15 @@ public class Word
     public string word;
     public string wordType; //verb, sustantive, adjective
     public List<string> wordUses = new List<string>(); //arreglo de usos de esa palabra, como por ejemplo, saludo, insulto, nombre, pregunta, etc (es un arreglo debido a que una palabra puede tener varios significados)
+    public Word(string _word, string _wordType, string[] _wordUses)
+    {
+        word = _word;
+        wordType = _wordType;
+        foreach (var wordUse in _wordUses)
+        {
+            wordUses.Add(wordUse);
+        }
+    }
 }
 [System.Serializable]
 public class Words
@@ -34,5 +43,17 @@ public class DatabaseCreation : MonoBehaviour
         }
         string emptyArray = JsonUtility.ToJson(newDictionary);
         File.WriteAllText(Application.dataPath + "/Data/Dictionary.json", emptyArray);
+    }
+    // public void UpdateDictionary(Words updatedDictionary)
+    // {
+    //     string updatedArray = JsonUtility.ToJson(updatedDictionary);
+    //     File.WriteAllText(Application.dataPath + "/Data/Dictionary.json", updatedArray);
+    // }
+
+    public void AddNewWordsToDiscover(string word, int hashValue)
+    {
+        string [] newWord = new string[1];
+        newWord[0] = $"word:{word}, hash:{hashValue}";
+        File.AppendAllLines(Application.dataPath + "/Data/NewWords.txt", newWord);
     }
 }
--- Scripts/Text Recongition/MessageRecongnition.cs
+++ Scripts/Text Recongition/MessageRecongnition.cs
@@ -6,6 +6,7 @@ public class MessageRecongnition : MonoBehaviour
 {
     public Words words;
     public TextAsset textJSON;
+    public DatabaseCreation databaseManager;
     public void ReadMessage(string message)
     {
         //supongamos caso "hola como estas"
@@ -14,14 +15,14 @@ public class MessageRecongnition : MonoBehaviour
         //luego se invocara la funcion ReadWord() que leera cada palabra y buscara en un archivo json lo que significa
         words = JsonUtility.FromJson<Words>(textJSON.text);
 
+        List<string> responses = new List<string>();
         foreach (var word in _words)
         {
-            ReadWord(word);
+            responses.Add(ReadWord(word));
         }
     }
-    private void ReadWord(string word)
+    private string ReadWord(string word)
     {
-        Debug.Log(word);
         //las palabras se guardaran en un json usando hash tables para reducir la complejidad algoritmica a la hora de buscar la palabra concreta. En el archivo json cada palabra estara guardada con su hash (por ejemplo hola => 104 111 108 97 => 420 lo cual dividido por la cantidad de entradas que tendra la tabla, nos da su indice)
         //luego una vez se acceda a esa palabra, se leera su informacion (por ejemplo, "hola" sera un objeto con informacion que diga que es un saludo, si puede ser una pregunta, si puede ser un verbo, etc) y en base a eso se sacaran las respuestas correspondientes
         int suma = 0;
@@ -33,10 +34,15 @@ public class MessageRecongnition : MonoBehaviour
         if (words.codes[suma].words.Find(_word => _word.word == word) != null)
         {
             Debug.Log("la palabra existe");
+            return "respuesta";
         }
         else
         {
-            Debug.Log("la palabra NO existe");
+            string[] uses = new string[1];
+            uses[0] = "unknown";
+            words.codes[suma].words.Add(new Word(word, "unknown", uses));
+            databaseManager.AddNewWordsToDiscover(word, suma);
+            return "newWordUnlocked";
         }
     }
 }
