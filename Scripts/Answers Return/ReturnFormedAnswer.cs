12ea46888abeb72731b9a0647b3b22d85808a204
Demian Ferreyra <demiancra@gmail.com>
2023-03-16 23:36:28 -0300

    [feature] responses to 'jergas' added

--- Scripts/Answers Return/ReturnFormedAnswer.cs
+++ Scripts/Answers Return/ReturnFormedAnswer.cs
@@ -14,8 +14,13 @@ public class ReturnFormedAnswer : MonoBehaviour
         //se partira en base a los : con un Split y se analizara ese texto
         string[] splitedStructure = structure.Split(":");
         string responseToAdd = "";
-        Debug.Log(splitedStructure[1]);
+        Debug.Log(splitedStructure[0]);
+        Debug.Log(structure);
 
+        if (splitedStructure[0] == "jerga" && splitedStructure.Length > 1)
+        {
+            responses.Add(GetRandomOptionBasedOnKeys("jergas", splitedStructure[1]));
+        }
         if (splitedStructure[0] == "greeting")
         {
             if (splitedStructure.Length > 2 && splitedStructure[2] == "user")
@@ -33,7 +38,6 @@ public class ReturnFormedAnswer : MonoBehaviour
             else if (splitedStructure.Length == 2)
             {
                 float getBool = Random.Range(0, 2);
-                Debug.Log(getBool);
                 if (getBool == 0)
                 {
                     responses.Add(GetRandomOption("greetings", splitedStructure[1]));
@@ -58,6 +62,37 @@ public class ReturnFormedAnswer : MonoBehaviour
                 foreach (var answer in intent.answers)
                 {
                     if (answer.specificIntent == specificIntent)
+                    {
+                        return answer.options[Random.Range(0, answer.options.Count)];
+                    }
+                }
+            }
+            else
+            {
+                intent = JsonUtility.FromJson<Answers>(File.ReadAllText(Application.dataPath + $"/Data/Common Answers/unableToAnswer.json"));
+                if (intent != null)
+                {
+                    return intent.answers[0].options[Random.Range(0, intent.answers[0].options.Count)];
+                }
+            }
+            return "wow, parece que lograron romperme... no se me ocurrio una forma de responder a esa pregunta... levantare un informe a Demian para que vea que sucedio";
+        }
+        catch (System.Exception)
+        {
+            return "wow, parece que lograron romperme... no se me ocurrio una forma de responder a esa pregunta... levantare un informe a Demian para que vea que sucedio";
+            throw;
+        }
+    }
+     private string GetRandomOptionBasedOnKeys(string fileToOpen, string specificKey)
+    {
+        try
+        {
+            intent = JsonUtility.FromJson<Answers>(File.ReadAllText(Application.dataPath + $"/Data/Common Answers/{fileToOpen}.json"));
+            if (intent != null)
+            {
+                foreach (var answer in intent.answers)
+                {
+                    if (answer.keys.Contains(specificKey))
                     {
                         return answer.options[Random.Range(0, answer.options.Count)];
                     }
